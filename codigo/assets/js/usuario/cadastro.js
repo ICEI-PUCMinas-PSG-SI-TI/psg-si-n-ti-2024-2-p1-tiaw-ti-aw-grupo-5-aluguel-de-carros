27e47207186f58ef68b4a0918445632e9c1add94
Heloisa Oliveira <lola.waldorf3@gmail.com>
2024-10-26 21:06:34 -0300

    Ajustes finais de layout e responsividade

--- codigo/assets/js/usuario/cadastro.js
+++ codigo/assets/js/usuario/cadastro.js
@@ -1,12 +1,17 @@
 const formProprietario = document.getElementById('form-proprietario');
 const formMotorista = document.getElementById('form-motorista');
 const selecaoUsuario = document.getElementById('selecao-usuario');
-var formNavigation = document.getElementById('botoes');
+const formNavigation = document.getElementById('botoes');
+const areaForm = document.getElementById("areaForm");
 
 function MostrarFormulario(type) {
     // Ocultar a seção de seleção de usuário e mostrar o formulário correspondente
     selecaoUsuario.style.display = 'none';
     formNavigation.style.display = 'flex';
+
+    // Aumentar área do formulário
+    areaForm.classList.remove("w-50");
+    areaForm.classList.add("w-75");
 
     // Exibir o formulário correto
     if (type === 'proprietario') {
@@ -21,6 +26,11 @@ function Voltar() {
     formProprietario.style.display = 'none';
     formMotorista.style.display = 'none';
     selecaoUsuario.style.display = 'block';
+
+    
+    // Aumentar área do formulário
+    areaForm.classList.remove("w-75");
+    areaForm.classList.add("w-50");
 }
 
 function Continuar() {
